45da1801dbdedb1f0e3d60b5122f252d9fff6761
shmink <t-nicklin@hotmail.co.uk>
2017-09-18 18:01:13 +0100

    Added more cases for the main bot.js file, still testing different implementations of how I see this bot working. Repos.json has been added as way to store the repos wanted to be monitored.
    Signed-off-by: shmink <t-nicklin@hotmail.co.uk>

--- bot.js
+++ bot.js
@@ -1,39 +1,60 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var repo = require('./repos.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
+
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    if (message.substring(0, 1) == '-') { // If the first character of a string is '-' then it's a command
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'ping':
+            // -v | -version = Version number of commiter bot
+            case 'v':
+            case 'version':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: 'version 0.1'
                 });
             break;
-            // Just add any case commands if you want to..
+            // -n | -name = Name of repo added for commiter bot to work with
+            case 'name':
+            case 'n':
+                bot.sendMessage({
+                    to: channelID,
+                    message: repo.name
+                });
+            break;
+            // -l | -link = Link of the repo added
+            case 'link':
+            case 'l':
+                bot.sendMessage({
+                    to: channelID,
+                    message: repo.link
+                });
+            break;
          }
      }
 });
